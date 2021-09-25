d09d7928133ec632508f1a0f13afe02b03459e4d
somacode <silviofrancoxa8@gmail.com>
2021-09-24 22:17:12 -0500

    prepare to production

--- gradle/bash/install.sh
+++ gradle/bash/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker network create books
-docker run --name booksdb -d --network books -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=booksdb -v /root/books/mysql:/var/lib/mysql --restart always mariadb --character-set-server=utf8 --collation-server=utf8_general_ci
-docker run --name booksmongo -d --network books -p 27017:27017 -v /root/books/mongo/:/data/db -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=1234 --restart always mongo
+docker network create somacode
+docker run --name booksdb -d --network somacode -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=booksdb -v /root/books/mysql:/var/lib/mysql --restart always mariadb --character-set-server=utf8 --collation-server=utf8_general_ci
+docker run --name booksmongo -d --network somacode -p 27017:27017 -v /root/books/mongo/:/data/db -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=1234 --restart always mongo
 
 #docker run --name ${project}mongoclient -d -p 3000:3000 --restart always --link ${project}mongo mongoclient/mongoclient
